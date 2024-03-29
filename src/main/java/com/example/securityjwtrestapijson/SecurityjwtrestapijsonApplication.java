330ea97bad2c75a8ade7b2d9fe3fb9b71d9ccc49
s0nnyday <lijnar100@naver.com>
2024-03-29 22:44:43 +0900

    Initial commit

---
+++ src/main/java/com/example/securityjwtrestapijson/SecurityjwtrestapijsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtrestapijson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityjwtrestapijsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityjwtrestapijsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityjwtrestapijson/SecurityjwtrestapijsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtrestapijson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityjwtrestapijsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
